29191ceb59bd8881b90aff112e87a13ae7a9c935
tcosse <tcosse@student.42.fr>
2020-06-25 15:46:15 +0200

    strncmp corriger

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tcosse <tcosse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 14:48:54 by tcosse            #+#    #+#             */
-/*   Updated: 2020/06/16 15:19:14 by tcosse           ###   ########.fr       */
+/*   Updated: 2020/06/25 15:45:50 by dwi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,15 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	c1 = *s1;
 	c2 = *s2;
-	while (--n && *s1 && *s2 && c1 == c2)
+	while (n-- && *s1 && *s2 && c1 == c2)
 	{
-		s1++;
-		s2++;
 		c1 = *s1;
 		c2 = *s2;
+		if (n)
+		{
+			s1++;
+			s2++;
+		}
 	}
-	if (n == 0)
-		return (0);
 	return (c1 - c2);
 }
