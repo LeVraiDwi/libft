69ece04f827cbe7354cd12ea1a9d030170328f3b
Theo COSSE <tcosse@e3r3p21.42.fr>
2020-06-25 16:05:03 +0200

    dsad

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tcosse <tcosse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 14:48:54 by tcosse            #+#    #+#             */
-/*   Updated: 2020/06/25 15:45:50 by dwi              ###   ########.fr       */
+/*   Updated: 2020/06/25 16:04:01 by tcosse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,17 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	c1 = *s1;
 	c2 = *s2;
-	while (n-- && *s1 && *s2 && c1 == c2)
+	while (n-- && *s1 && *s2)
 	{
-		c1 = *s1;
-		c2 = *s2;
+		if (c1 != c2)
+			return (c1 - c2);
 		if (n)
 		{
 			s1++;
 			s2++;
 		}
+		c1 = *s1;
+		c2 = *s2;
 	}
 	return (c1 - c2);
 }
